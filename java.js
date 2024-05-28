8ab126348a6a27b3fb53511ea15f727eef71d8f5
Abdelatifshj <abdelatifsenhadji4@gmail.com>
2024-05-28 18:59:20 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -6,29 +6,35 @@ const age = document.getElementById("age");
 const height = document.getElementById("hi");
 const weight = document.getElementById("we");
 const activityLevel = document.getElementById("activityLevel");
-const goal = document.getElementById("goal");
+const goal = document.getElementById("goal");//retuen un objet li 3andah hada id 
 
 function calculateBMR(weight, height, age, gender) {
     if (gender === "male") {
         return 10 * weight + 6.25 * height - 5 * age + 5;
+    } else if (gender === "female") {
+        return 10 * weight + 6.25 * height - 5 * age - 161;
+    } else {
+        
+        return NaN; 
     }
-    return 10 * weight + 6.25 * height - 5 * age - 161;
 }
 
+
 calculateBtn.addEventListener("click", () => {
-    let genderValue = document.querySelector(".calories-cal input[name='gender']:checked").value;
-    let weightValue = parseFloat(weight.value);
+    let genderValue = document.querySelector(".calories-cal input[name='gender']:checked").value;//select male or female an reseviha dans cette variable
+    let weightValue = parseFloat(weight.value);//convert un string to float w resrvih dans cette var
     let heightValue = parseFloat(height.value);
     let ageValue = parseFloat(age.value);
     let goalValue = goal.value;
 
     if (isNaN(weightValue) || isNaN(heightValue) || isNaN(ageValue)) {
-        calories.innerHTML = "Please enter valid numbers for weight, height, and age.";
+        calories.innerHTML = "Please enter validparseFloat numbers for weight, height, and age.";
         return;
     }
 
     let BMR = calculateBMR(weightValue, heightValue, ageValue, genderValue);
-    let activityMultiplier = parseFloat(activityLevel.value);
+
+    let activityMultiplier = (activityLevel.value);
 
     if (isNaN(activityMultiplier)) {
         calories.innerHTML = "Please select your activity level.";
@@ -36,8 +42,8 @@ calculateBtn.addEventListener("click", () => {
     }
 
     let TDEE = BMR * activityMultiplier;
+
     let dailyCaloriesMin, dailyCaloriesMax;
-    let message;
 
   if (goalValue === "maintain") {
     dailyCaloriesMin = TDEE - 100;
